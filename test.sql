183cee9a33e2ffc8105d18dd7ce2488ed5ead5b7
914-dot <yuju980505@gmail.com>
2026-06-04 15:53:10 +0800

    6/4筆記-2

--- test.sql
+++ test.sql
@@ -1,17 +1,5 @@
-select *, 
-    case
-        when fee > 500 then fee * 0.8
-        when fee > 300 then fee *0.9
-        else fee
-    end as discount
-from Bill
---where fee >= 300 and fee <= 500
---where fee between 300 and 500
---where fee not between 300 and 500
-where fee < 300 or fee > 500
-order by fee, hid
-
-select cname
-from UserInfo
-where cname <> ''and cname is not null
-order by cname
+select UserInfo.uid, cname, address
+from UserInfo, Live, House
+where UserInfo.uid = Live.uid
+    and Live.hid = House.hid
+    and cname = '王大明'
