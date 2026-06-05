8b47e5164ca8b97f7344cbb1a2e11dc5c50b0e32
914-dot <yuju980505@gmail.com>
2026-06-05 10:47:34 +0800

    6/5 group練習-2

--- test.sql
+++ test.sql
@@ -1,3 +1,4 @@
-select tel, count(*), avg(fee), sum(fee), max(fee), min(fee)
-from Bill
-group by tel
+select Bill.tel, address, count(*), avg(fee), sum(fee), max(fee), min(fee)
+from Bill, Phone, House
+where Bill.tel = Phone.tel and Phone.hid = House.hid
+group by Bill.tel, address
