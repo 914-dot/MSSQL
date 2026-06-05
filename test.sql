96f86401889348c6b310fa3a2a1823bebfc47b42
914-dot <yuju980505@gmail.com>
2026-06-05 14:43:16 +0800

    查詢資料年份

--- test.sql
+++ test.sql
@@ -1,18 +1,2 @@
--- ============== 電話帳單金額總和的極端直查詢　===============
-select *
-from (
-    SELECT
-        tel,
-        SUM(fee) AS total_fee
-    FROM Bill
-    GROUP BY tel
-) as x
-where total_fee = (
-    select max(total_fee) as min_total_fee
-    from (
-        SELECT
-            SUM(fee) AS total_fee
-        FROM Bill
-        GROUP BY tel
-    ) as x
-)
+select distinct year(dd) from Bill
+-- 查詢資料年份
