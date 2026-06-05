2bf073c0a6e1bb97a20b185c0f33cb3f2ef69180
914-dot <yuju980505@gmail.com>
2026-06-05 14:24:52 +0800

    取姓氏查詢

--- test1.sql
+++ test1.sql
@@ -1,13 +1,4 @@
-
-select * from UserInfo, Live where UserInfo.uid = Live.uid and cname = '王大明'
--- 看王大明住在哪間屋子
-
-select UserInfo.uid, cname
-from UserInfo, Live
-where  UserInfo.uid = Live.uid and
-    hid in (
-        select hid from UserInfo, Live where UserInfo.uid = Live.uid and cname = '王大明'
-    )
--- ↑跟王大明住在同一間屋子的人
-    and cname <> '王大明'
--- 跟王大明住在同一間屋子的人有誰(and<>不包含王大明)
+select distinct left(cname, 1) as lastname
+-- 取姓氏(取左邊第一個字)
+from UserInfo
+where cname is not null and cname <> ''
