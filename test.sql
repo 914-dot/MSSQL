a804c5753184c03e3f6d5a0b754c820a16cad1e1
914-dot <yuju980505@gmail.com>
2026-06-08 10:05:05 +0800

    Epoch Time

--- test.sql
+++ test.sql
@@ -1,20 +1,4 @@
-select distinct year(dd) from Bill
--- 查詢資料年份
+select datediff(s, '1970/1/1', getutcdate())
 
-select *, concat(
-    '中華民國',
-    year(dd) - 1911,
-    format(dd, '年M月d日')
-)
-from Bill
--- 轉換中華民國
-
-select datetrunc(year, getdate())
-select datetrunc(month, getdate())
-select datetrunc(week, getdate())
-select datetrunc(iso_week, getdate())
--- 去零頭
-
-select * from Bill
-where dd between '2019/1/1' and '2019/3/1 23:59:59.999'
--- 查詢特定時段資料
+select dateadd(s, 1780883980, '1970/1/1') + 8 / 24.0
+-- Epoch Time 以 1970/1/1 0:0:0 為基準到現在經過了多少秒，並且為 UTC 時間
