f0733eefbebf0c006f943a7f42eef5e6472d9c05
914-dot <yuju980505@gmail.com>
2026-06-08 09:57:55 +0800

    between 查詢特定時段資料

--- test.sql
+++ test.sql
@@ -14,3 +14,7 @@ select datetrunc(month, getdate())
 select datetrunc(week, getdate())
 select datetrunc(iso_week, getdate())
 -- 去零頭
+
+select * from Bill
+where dd between '2019/1/1' and '2019/3/1 23:59:59.999'
+-- 查詢特定時段資料
